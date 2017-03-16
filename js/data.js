d73e2604e5d83056d105bad7150563c303d3678c
MU\Administrator <416018952@qq.com>
2017-03-16 09:29:17 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,326 @@
+var data = [
+    [{
+            id: '001',
+            goodsImg: '',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '000000000168671413',
+            goodsSale: '',
+            goodsPrice: '',
+            goodsLink: ''
+        },
+        {
+            id: '002',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '000000000107028503',
+            goodsSale: '',
+            goodsPrice: '',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '003',
+            goodsImg: 'images/168671413.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '000000000142813238',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '004',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '000000000603480020',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '005',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '006',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '007',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '008',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        }
+    ],
+    [{
+            id: '001',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '002',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '003',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '004',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '005',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '006',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '007',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '008',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        }
+    ],
+    [{
+            id: '001',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '002',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '003',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '004',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '005',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '006',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '007',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '008',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        }
+    ],
+    [{
+            id: '001',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '002',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '003',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '004',
+            goodsImg: 'https://image.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128731998_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '005',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '006',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '007',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        },
+        {
+            id: '008',
+            goodsImg: 'http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000168671413_1_400x400.jpg',
+            goodsName: '海尔（Haier） 3匹 智能 除甲醛 二级能效空调柜机 KFR-72LW/10UBC12U1',
+            goodsDescription: '【314海尔超级品牌日，优惠敬请期待】【好评晒单返券100元】二级节能，智能智控，甲醛克星 ',
+            goodsCode: '',
+            goodsSale: '99.99',
+            goodsPrice: '129.00',
+            goodsLink: 'http://product.suning.com/168671413.html'
+        }
+    ]
+];
